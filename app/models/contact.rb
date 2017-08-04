7b188bb6ba6fcfdbba866217bc37acbbf1d6de60
Nicolas Hunter <nhunt006@fiu.edu>
2017-08-04 02:38:07 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+    # Form Validation. If not present. Wont save to DB
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end

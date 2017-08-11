25550ee8c70bdeac40beee9de4cfb2b46517d6a6
Nicolas Hunter <nhunt006@fiu.edu>
2017-08-11 03:12:59 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user! # Authenticates user before any actions can occur.(Devise)
     
     def index
-        
+       @users=User.includes(:profile) # Less queries that User.all. Faster
     end
     
     # GET to /users/:id
